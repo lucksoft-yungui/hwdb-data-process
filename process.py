9051ee053c4a35f7dc767956a1917d9de0ca67aa
maizi <1319634411@qq.com>
2023-06-19 15:14:26 +0800

    bug fix

--- process.py
+++ process.py
@@ -5,6 +5,8 @@ import cv2
 import numpy as np
 from PIL import Image
 
+current_index = 0
+
 def gb2312_to_decimal(gb2312_char):
     return int(gb2312_char.encode('gb2312').hex(), 16)
 
@@ -33,7 +35,8 @@ def random_resize(image, max_dim):
     return new_image
 
 def generate_dataset(words_file, char_images, output_dir, num_samples, label_file, max_length, min_spacing, max_spacing):
-    
+    global current_index
+
     os.makedirs(os.path.join(output_dir, "labels"), exist_ok=True)
 
     with open(words_file, 'r', encoding='utf-8') as f:
@@ -44,6 +47,7 @@ def generate_dataset(words_file, char_images, output_dir, num_samples, label_fil
     label_file_path = os.path.join(output_dir, "labels", label_file)
     with open(label_file_path, 'w', encoding='utf-8') as f:
         for i in range(num_samples):
+            index = current_index + i
             word = random.choice(words)
 
             if len(word) > max_length:
@@ -73,15 +77,16 @@ def generate_dataset(words_file, char_images, output_dir, num_samples, label_fil
                 blank_image[start_y:start_y+img.shape[0], x:x+img.shape[1]] = img
                 x += img.shape[1] + spacing
             
-            img_path = os.path.join(output_dir, "imgs", f"{i}.png")
+            img_path = os.path.join(output_dir, "imgs", f"{index}.png")
             cv2.imwrite(img_path, blank_image)
             
-            f.write(f"{i}.png {word}\n")
+            f.write(f"{index}.png {word}\n")
+        current_index += num_samples
 
 if __name__ == "__main__":
     train_images = get_char_images("/Users/peiyandong/Documents/code/ai/train-data/hwdb/single/1.0/raw/train")
     test_images = get_char_images("/Users/peiyandong/Documents/code/ai/train-data/hwdb/single/1.0/raw/test")
 
     generate_dataset("data/words.txt", train_images, "/Users/peiyandong/Documents/code/ai/train-data/hwdb/single/data", 10000, "hwdb_train.txt", 7, 0, 10)
-    generate_dataset("data/words.txt", test_images, "/Users/peiyandong/Documents/code/ai/train-data/hwdb/single/data", 500, "hwdb_val.txt", 7, 0, 10)
+    generate_dataset("data/words.txt", train_images, "/Users/peiyandong/Documents/code/ai/train-data/hwdb/single/data", 500, "hwdb_val.txt", 7, 0, 10)
     generate_dataset("data/words.txt", test_images, "/Users/peiyandong/Documents/code/ai/train-data/hwdb/single/data", 1000, "hwdb_test.txt", 7, 0, 10)
